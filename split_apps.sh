d82d76447f21b70ffdc5795d2983a7278ac53fab
Paul Posch <Pawlovicky@gmail.com>
2018-09-08 18:06:27 +0000

    Fixed tag splitting in awk. Now the patent-applications are splitted alongs their XML tag

--- split_apps.sh
+++ split_apps.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 name=`echo "$1" | cut -d'.' -f1`
 mkdir $name
-awk -v x="F_0.xml" "/<\/us-patent-application>/ "'{ print "</us-patent-application>" > x; x="F_"++i".xml";next}{ print > x; }' $1
+awk -v x="F_0.xml" "/<us-patent-application*/, /<\/us-patent-application>/"'{ print  > x; } /<\/us-patent-application>/ {close(x); x="F_"++i".xml"}' $1
 mv *_*.xml $name
 rm $1
